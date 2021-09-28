8d3920f3416f45e9fe0432e58877618ce8466674
rileydevine <824055@mystma.org>
2021-09-28 09:13:11 -0500

    submitted

--- main.py
+++ main.py
@@ -1,1 +1,16 @@
 #WRITE YOUR CODE IN THIS FILE
+
+#define function
+def password(p):
+
+    #correct password
+    if p == "Knights19":
+        return "ACCESS GRANTED"
+
+    #else for incorrect password
+    else:
+        return "ACCESS DENIED"
+
+#run password
+print(password("password"))
+    
